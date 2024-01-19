94cf68acb4c94d10543b1d24812c262b19293366
jean <jeanbrunemello@gmail.com>
2024-01-19 00:40:58 -0300

    adição de dados em seeders

--- seeders/20240113005019-listas-compras.js
+++ seeders/20240113005019-listas-compras.js
@@ -4,14 +4,22 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
 
-    await queryInterface.bulkInsert('listas_compras', [{
-      nome_lista: 'lista 1',
-    }], {});
+    await queryInterface.bulkInsert('listas_compras', [
+      {
+        nome_lista: 'Mercado',
+      },
+      {
+        nome_lista: 'Petshop',
+      },
+      {
+        nome_lista: 'Feira da fruta',
+      }
+    ], {});
   },
 
   async down(queryInterface, Sequelize) {
 
     await queryInterface.bulkDelete('listas_compras', null, {});
-    
+
   }
 };
--- seeders/20240113012936-produtos.js
+++ seeders/20240113012936-produtos.js
@@ -7,15 +7,51 @@ module.exports = {
     await queryInterface.bulkInsert('produtos', [
       {
         nome_produto: 'arroz',
-        quantidade_produto: 2,
+        quantidade_produto: 1,
         preco_produto: 9.99,
         listas_compras_id: 1
       },
       {
         nome_produto: 'feijão',
-        quantidade_produto: 1,
+        quantidade_produto: 2,
         preco_produto: 4.58,
         listas_compras_id: 1
+      },
+      {
+        nome_produto: 'coca-coa',
+        quantidade_produto: 3,
+        preco_produto: 12.99,
+        listas_compras_id: 1
+      },
+      {
+        nome_produto: 'orelha de porco desidratada',
+        quantidade_produto: 3,
+        preco_produto: 6.99,
+        listas_compras_id: 2
+      },
+      {
+        nome_produto: 'bravecto',
+        quantidade_produto: 1,
+        preco_produto: 199.99,
+        listas_compras_id: 2
+      },
+      {
+        nome_produto: 'maçã',
+        quantidade_produto: 5,
+        preco_produto: 5.55,
+        listas_compras_id: 3
+      },
+      {
+        nome_produto: 'melancia',
+        quantidade_produto: 1,
+        preco_produto: 9.99,
+        listas_compras_id: 3
+      },
+      {
+        nome_produto: 'morango',
+        quantidade_produto: 1,
+        preco_produto: 4.58,
+        listas_compras_id: 3
       }
     ], {});
 
